c038695a5fd4abba19fd25e4f9631339bea3a548
Guilherme Cirne <gcirne@gmail.com>
2012-01-25 23:25:06 -0200

    specs skeleton.

---
+++ spec/acceptance/wip_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+feature "WIP" do
+
+  scenario "Rails env test" do
+    visit "/"
+  end
+
+end
---
+++ spec/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '6eedc3cc5ae951b2b6e7900a81a0fc2d2b27b76bd837e9dddc3dd0ade3df4dd2d0f4f31bd06424a56077c965c586a4690939b8eedaa265158bc9717995c0a1c9'
